da8e77bf84214a6b87e0a7cf6413de9d0a16b9eb
Sagar Gupta <sagar_gupta_1@live.com>
2020-12-05 18:19:38 +0530

    CountDown

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,51 @@
 import React, { Component, useState, useEffect } from "react";
 import '../styles/App.css';
 
-const App = () => {
-  // write your code here 
-
-  return (
-    <div className="wrapper">
-      <div id="whole-center">
-        <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
-        </h1>
+// const App = () => {
+class App extends React.Component{
+  // write your code here
+  constructor(){ 
+    super();
+    this.timeInterval = null;
+    this.state = {time: 0};
+    this.start = this.start.bind(this); 
+  } 
+  start(){
+      this.timeInterval = setInterval(
+        ()=> this.setState({time:this.state.time - 1}), 1000);
+  }
+  componentDidUpdate(){
+    if(this.state.time === 0){
+      clearInterval(this.timeInterval);
+    }
+  }
+  render(){
+    return (
+      <div className="wrapper">
+        <div id="whole-center">
+          <h1>
+            Reverse countdown for<input id="timeCount" 
+            onKeyDown={
+              (evt)=>{
+                clearInterval(this.timeInterval);
+                if(evt.keyCode === 13){
+                const val = document.getElementById("timeCount").value;
+                document.getElementById("timeCount").value = "";
+                if(isNaN(val)){
+                  this.setState({time:0});
+                }
+                else{
+                  this.setState({time:parseInt(val)});
+                }
+                this.start();
+              } 
+            }} /> sec.
+          </h1>
+        </div>
+        <div id="current-time">{this.state.time}</div>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
-    </div>
-  )
+    )
+  }    
 }
 
 
